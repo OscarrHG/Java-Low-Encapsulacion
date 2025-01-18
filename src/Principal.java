ba3be34942699ba4a1d5d6a37d1d38f7f856068d
OscarrHG <hgoscar3@gmail.com>
2025-01-17 22:51:17 -0500

    Encapsulacion 1era

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+import Prueba.Persona;
+
+public class Principal {
+    public static void main(String[] args) {
+        Persona p1 = new Persona();
+        p1.dimeNombre();
+    }
+}
---
+++ src/Prueba/Persona.java
@@ -0,0 +1,9 @@
+package Prueba;
+
+public class Persona {
+    String nombre = "Pedro";
+
+    public void dimeNombre(){
+        System.out.println(nombre);
+    }
+}
